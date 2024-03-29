0723866bbe9d9340c398b3e09b1b7b9232f3a9c0
Ben Davies <bdavies6086@hotmail.co.uk>
2024-03-29 13:41:38 +0000

    Iniital cpu module

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,13 +1,48 @@
+mod operations;
+mod register;
+mod arithmetic;
 
+use register::*;
+use arithmetic::add;
 
-pub fn runOp(register: &Register, op: u8) {
-    match op(
+const MAX_CYCLES: i32 = 69905;
 
-        0x00-> 
+pub struct CPU {
+    registers: Register,
+    flags: u8,
+    h_ram: [u8; 127]
+}
 
+impl CPU {
 
-        _->panic!("No matching op for ${op}")
-    )
+    pub fn new() -> Self {
+        return Self {
+            registers: Register {
+                acc: 0,
+                b: 0,
+                c: 0,
+                d: 0,
+                e: 0
+            },
+            flags: 0,
+            h_ram: [0; 127]
+        }
+    }
 
+    pub fn tick(&mut self) {
+        let mut cycle_count = 0;
 
+        while cycle_count < MAX_CYCLES {
+            cycle_count = cycle_count + 1;
+        }
+
+    }
+
+}
+
+fn run_op(op: u8, register: &mut Register, flags: &mut u8) -> (u8, u8) {
+    return match op {
+        0x00 => noop(),
+        _=> panic!("unknown op")
+    };
 }
---
+++ src/cpu/register.rs
@@ -0,0 +1,8 @@
+pub struct Register {
+    pub acc: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    
+}
