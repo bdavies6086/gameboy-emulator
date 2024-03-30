e1c8f982f3190ee9edaa7481806a94a038ab7f95
Ben Davies <bdavies6086@hotmail.co.uk>
2024-03-30 09:37:48 +0000

    Implement all flag checks excluding half carry

--- src/cpu/arithmetic.rs
+++ src/cpu/arithmetic.rs
@@ -7,59 +7,67 @@ pub struct Flags {
 bitflags! {
     pub struct FlagOps: u32 {
         const Zero = 0x01;
-        const Subtract = 0x02;
-        const HalfCarry = 0x04;
-        const Carry = 0x08;
-        const ForceZeroOn = 0x10;
-        const ForceZeroOff = 0x20;
-        const ForceSubtractOn = 0x40;
-        const ForceSubtractOff = 0x80;
-        const ForceHalfCarryOn = 0x100;
-        const ForceHalfCarryOff = 0x200;
-        const ForceCarryOn = 0x400;
-        const ForceCarryOff = 0x800;
+        const HalfCarry = 0x02;
+        const Carry = 0x04;
+        const ForceZeroOn = 0x8;
+        const ForceZeroOff = 0x10;
+        const ForceSubtractOn = 0x20;
+        const ForceSubtractOff = 0x40;
+        const ForceHalfCarryOn = 0x80;
+        const ForceHalfCarryOff = 0x100;
+        const ForceCarryOn = 0x200;
+        const ForceCarryOff = 0x400;
     }
 }
 
 const ADD_8_BIT_FLAGS: FlagOps = FlagOps::Zero | FlagOps::ForceSubtractOff | FlagOps::HalfCarry | FlagOps::Carry;
 
 impl Flags {
-    pub fn update_flags(self: &mut Self, result: Option<u8>, updates: FlagOps) {
+    pub fn update_flags(self: &mut Self, overflowed: bool, updates: FlagOps, a: u8, b: u8, result: u8) {
         
-         
-        if (updates.contains(FlagOps::Zero)) {
+        // Todo this feels messy with nested ifs and duplication, clean up.
+        if (updates.intersects(FlagOps::Zero)) {
+            if(result == 0) {
+                self.flags = self.flags | 0x80;
+            }
+            else {
+                self.flags = self.flags & 0x70;
+            }
+        }
+        // Todo come back to this, I'm hoping it only affects the DAA for now
+        if (updates.intersects(FlagOps::HalfCarry)){
 
         }
-        if (updates.contains(FlagOps::Subtract)){}
-        if (updates.contains(FlagOps::HalfCarry)){}
-        if (updates.contains(FlagOps::Carry)){}
-        if (updates.contains(FlagOps::ForceZeroOn)){
+        if (updates.intersects(FlagOps::Carry)){
+            match overflowed {
+                true => { self.flags = self.flags | 0x10 },
+                false => { self.flags = self.flags & 0xe0 }
+            }
+        }
+        if (updates.intersects(FlagOps::ForceZeroOn)){
             self.flags = self.flags | 0x80;
         }
-        if (updates.contains(FlagOps::ForceZeroOff)){
+        if (updates.intersects(FlagOps::ForceZeroOff)){
             self.flags = self.flags & 0x70;
         }
-        if (updates.contains(FlagOps::ForceSubtractOn)){
+        if (updates.intersects(FlagOps::ForceSubtractOn)){
             self.flags = self.flags | 0x40;
         }
-        if (updates.contains(FlagOps::ForceSubtractOff)){
+        if (updates.intersects(FlagOps::ForceSubtractOff)){
             self.flags = self.flags & 0xb0;
         }
-        if (updates.contains(FlagOps::ForceHalfCarryOn)){
+        if (updates.intersects(FlagOps::ForceHalfCarryOn)){
             self.flags = self.flags | 0x20;
         }
-        if (updates.contains(FlagOps::ForceHalfCarryOff)){
+        if (updates.intersects(FlagOps::ForceHalfCarryOff)){
             self.flags = self.flags & 0xd0;
         }
-        if (updates.contains(FlagOps::ForceCarryOn)){
+        if (updates.intersects(FlagOps::ForceCarryOn)){
             self.flags = self.flags | 0x10;
         }
-        if (updates.contains(FlagOps::ForceCarryOff)){
+        if (updates.intersects(FlagOps::ForceCarryOff)){
             self.flags = self.flags & 0xe0;
         }
-
-
-
     }
 }
 
@@ -77,13 +85,13 @@ fn set_carry(res: Option<u8>) -> bool {
     }
 }
 
-pub fn add(a: u8, b: u8, flags: &mut Flags) -> Option<u8> {
+pub fn add(a: u8, b: u8, flags: &mut Flags) -> u8 {
 
-    let res = a.checked_add(b);
+    let res = a.overflowing_add(b);
 
-    flags.update_flags(res, ADD_8_BIT_FLAGS);
+    flags.update_flags(res.1, ADD_8_BIT_FLAGS, a, b, res.0);
 
-    return res;
+    return res.0;
 }
 
 pub fn sub(a: u8, b: u8) -> Option<u8> {
